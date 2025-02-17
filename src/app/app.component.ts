c8721566a0094cf545b1354ca0ae27b333acbbb6
Dominika Warchoł <242408@student.pwr.edu.pl>
2025-02-17 18:53:44 +0100

    dsfsdfsdf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {CrisisListComponent} from "./crisis-list/crisis-list.component";
+import {HeroesListComponent} from "./heroes-list/heroes-list.component";
 
 @Component({
   selector: 'app-root',
